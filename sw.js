d45a140888ed6b646b8bfac9607dad23fef01450
Jibril6919 <jibrilbenkmicha@gmail.com>
2026-08-23 13:45:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fonte-v103";
+const CACHE = "fonte-v104";
 const ASSETS = ["./", "index.html", "app.js", "manifest.webmanifest", "icon-192.png", "icon-512.png", "apple-touch-icon.png", "favicon-32.png"];
 
 self.addEventListener("install", (e) => {
